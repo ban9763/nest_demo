1c59ef003a2287466f944470f5724f054a60df6b
ban123 <2248993992@qq.com>
2024-09-22 17:23:12 +0800

    添加注释与路由

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('/api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
+    console.log('hello 收到请求')
     return this.appService.getHello();
+  }
+
+  @Get('/boy')
+  getBoy():string{
+    console.log('boy 收到消息');
+    return this.appService.getBoy();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
-
+// 依赖注入
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+  getBoy(): string {
+    return 'Hello boy!';
   }
 }
