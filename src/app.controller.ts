c3c3c6b0f514138b7c1392cfb0a34351cee1fee0
ban123 <2248993992@qq.com>
2024-09-29 23:08:39 +0800

    完成：post,get,put,delete的demo

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller('/api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    console.log('hello 收到请求')
+  @Get('/user/:id')
+  getHello(@Req() req): string {
+    console.log('hello 收到请求',req.params)
     return this.appService.getHello();
   }
 
-  @Get('/boy')
-  getBoy():string{
-    console.log('boy 收到消息');
+  @Get('/boy/*/name/*')
+  getBoy(@Req() req):string{
+    console.log('boy 收到消息',req.params);
     return this.appService.getBoy();
   }
 }
