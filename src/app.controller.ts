2f8c0eeb16acba7888f3c1bcfb0570a53a6090bf
ban9763 <2248993992@qq.com>
2025-01-20 17:07:01 +0800

    req，路由通配符

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,6 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return 'app的服务';
+    return 'app的服务123';
   }
 }
--- src/user_module/user.controller.ts
+++ src/user_module/user.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, Ip, Req } from '@nestjs/common';
 
 @Controller('/api')
 export class UserController {
   constructor() {}
 
-  @Get('/user')
-  getHello(@Req() req): string {
-    console.log('hello 收到请求', req.params);
+  @Get('/user/:id')
+  getHello(@Req() req, @Ip() ip): string {
+    console.log('收到请求header', req.headers);
+    console.log('收到请求params', req.params);
+    console.log('收到请求body', req.body);
+    console.log('收到请求ip', ip);
     return '获取用户11';
+  }
+
+  @Get('ab*cd')
+  findAll() {
+    return 'This route uses a wildcard';
   }
 }
