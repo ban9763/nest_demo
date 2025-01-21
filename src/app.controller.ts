9a270d2ab145ebb07e22cbac30fae9225b58f80f
ban9763 <2248993992@qq.com>
2025-01-21 11:42:41 +0800

    采用对象的方式引用userService

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UserService } from './user_module/user.service';
 
 @Controller('/api')
 export class AppController {
   // 1. 第一种注入方式
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('userService') private readonly userService: UserService,
+  ) {}
 
   // 2. 第一种注入方式
   // private readonly appService: AppService;
@@ -15,5 +19,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return 'app的服务123';
+  }
+
+  @Get('/objectUser')
+  getUserService(): string {
+    return this.userService.getUser();
   }
 }
--- src/user_module/user.service.ts
+++ src/user_module/user.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class UserService {
   getUser(): string {
-    return '获取james';
+    return 'User获取james';
   }
 }
