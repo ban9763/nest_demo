4b9b1674f0f28721be78d09f1990a69cc3ea8223
ban9763 <2248993992@qq.com>
2025-01-21 15:27:18 +0800

    中间件next后对response的一些操作

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -6,6 +6,9 @@ export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     console.log('中间件 logger之前');
     next(); // 逻辑部分的处理
-    console.log('中间件 logger之后');
+    // 一般使用控制response的 http status
+    // 给浏览器setCookie
+    res.status(500);
+    console.log('中间件 logger之后', res.statusCode);
   }
 }
